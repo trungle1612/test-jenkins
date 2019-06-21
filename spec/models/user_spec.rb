a6f2cb302b8ce6e173b6b02cd1cdfdaf10c4688f
le xuan trung <trungle@xtaypro.com>
2019-06-21 10:35:28 +0700

    Test rspec

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe  "ActiveRecord associations" do
+    let!(:user) { User.new }
+    it { expect(user).to have_db_column(:last_name).of_type(:string) }
+    it { expect(user).to have_db_column(:first_name).of_type(:string) }
+  end
+end
