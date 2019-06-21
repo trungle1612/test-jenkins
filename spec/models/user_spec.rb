162d41b994c4435581bfc33cc39ff71613925875
le xuan trung <trungle@xtaypro.com>
2019-06-21 11:48:25 +0700

    udpate rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,5 @@ RSpec.describe User, type: :model do
     let!(:user) { User.new }
     it { expect(user).to have_db_column(:last_name).of_type(:string) }
     it { expect(user).to have_db_column(:first_name).of_type(:string) }
-    it { expect(user).to have_db_column(:first_name_kaka).of_type(:string) }
   end
 end
